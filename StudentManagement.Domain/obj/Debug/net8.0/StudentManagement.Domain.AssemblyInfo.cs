49f9ea852dc1d1092eb78bb04ec37c38e79383b2
Artem <shumckiy2004@gmail.com>
2024-03-02 21:54:54 +0200

    Gitiignor!

--- StudentManagement.Domain/obj/Debug/net8.0/StudentManagement.Domain.AssemblyInfo.cs
+++ StudentManagement.Domain/obj/Debug/net8.0/StudentManagement.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0844b2e510a733fdfa71ebcfc6aa08f989506547")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5327ac79994a4942180e4960ae7f4cf2782ce95")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
